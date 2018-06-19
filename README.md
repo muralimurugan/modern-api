94f4d9e45241edbe7902d8561070c9065969c8c0
Dhayanandhan <dhayaec@gmail.com>
2018-06-19 22:30:45 +0530

    add readme text

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# install dependencies by yarn
+#RUN yarn
+
+# http://localhost:4000 for entry page
+
+# http://localhost:4000/graphiql
+
+# http://localhost:4000/api/v1/paintings
--- index.js
+++ index.js
@@ -1,109 +1,108 @@
 // import { graphiqlHapi } from 'apollo-server-hapi';
-const{graphqlHapi,graphiqlHapi}=require('apollo-server-hapi');
-const graphql=require('graphql');
-const hapi=require('hapi');
-const mongoose=require('mongoose');
-const Painting=require('./models/Painting');
-const schema=require('./graphql/schema');
-mongoose.connect('mongodb://localhost/modern-api');
-mongoose.connection.once('open',()=>{
-    console.log('database connected');
-})
-const Inert = require('inert');
-const Vision = require('vision');
-const HapiSwagger = require('hapi-swagger');
-const Pack = require('./package');
+const {graphqlHapi, graphiqlHapi} = require ('apollo-server-hapi');
+const graphql = require ('graphql');
+const hapi = require ('hapi');
+const mongoose = require ('mongoose');
+const Painting = require ('./models/Painting');
+const schema = require ('./graphql/schema');
+mongoose.connect ('mongodb://localhost/modern-api');
+mongoose.connection.once ('open', () => {
+  console.log ('database connected');
+});
+const Inert = require ('inert');
+const Vision = require ('vision');
+const HapiSwagger = require ('hapi-swagger');
+const Pack = require ('./package');
 
-const server=hapi.server({
-    port:4000,
-    host:'localhost'
-})
+const server = hapi.server ({
+  port: 4000,
+  host: 'localhost',
+});
 
-const init=async()=>{
-    await server.register([
-		Inert,
-		Vision,
-		{
-			plugin: HapiSwagger,
-			options: {
-				info: {
-					title: 'Paintings API Documentation',
-					version: Pack.version
-				}
-			}
-		}
-	]);
-    await server.register({
-		plugin: graphiqlHapi,
-		options: {
-			path: '/graphiql',
-			graphiqlOptions: {
-				endpointURL: '/graphql'
-			},
-			route: {
-				cors: true
-			}
-		}
-	});
-
-	await server.register({
-		plugin: graphqlHapi,
-		options: {
-			path: '/graphql',
-			graphqlOptions: {
-				schema
-			},
-			route: {
-				cors: true
-			}
-		}
-	});
-    server.route([
-        {
-            method:'get',
-            path:'/',
-            config: {
-				description: 'home page',
-				tags: ['api', 'v1', 'painting']
-			},
-            handler:function(request,reply){
-                return '<h1>MY TEST API welcome murali!</h1>';
-            }
+const init = async () => {
+  await server.register ([
+    Inert,
+    Vision,
+    {
+      plugin: HapiSwagger,
+      options: {
+        info: {
+          title: 'Paintings API Documentation',
+          version: Pack.version,
         },
-        {
-            method:'get',
-            path:'/api/v1/paintings',
-            config: {
-				description: 'Get all the paintings',
-				tags: ['api', 'v1', 'painting']
-			},
-            handler:function(request,reply){
-                return  Painting.find();
-            }
-        },
-        
-        {
-            method:'post',
-            path:'/api/v1/paintings',
-            config: {
-				description: 'post painting',
-				tags: ['api', 'v1', 'painting']
-			},
-            handler:function(request,reply){
-                const {name,url,techniques}=request.payload;
-                const painting=new Painting({
-                    name,
-                    url,
-                    techniques
-                })
-                return  painting.save();
-            }
-        }
-    ])
-    
+      },
+    },
+  ]);
+  await server.register ({
+    plugin: graphiqlHapi,
+    options: {
+      path: '/graphiql',
+      graphiqlOptions: {
+        endpointURL: '/graphql',
+      },
+      route: {
+        cors: true,
+      },
+    },
+  });
 
-    await server.start();
-    console.log(`server running on ${server.info.uri}`);
-}
+  await server.register ({
+    plugin: graphqlHapi,
+    options: {
+      path: '/graphql',
+      graphqlOptions: {
+        schema,
+      },
+      route: {
+        cors: true,
+      },
+    },
+  });
+  server.route ([
+    {
+      method: 'get',
+      path: '/',
+      config: {
+        description: 'home page',
+        tags: ['api', 'v1', 'painting'],
+      },
+      handler: function (request, reply) {
+        return '<h1>welcome to the entry point of api</h1>';
+      },
+    },
+    {
+      method: 'get',
+      path: '/api/v1/paintings',
+      config: {
+        description: 'Get all the paintings',
+        tags: ['api', 'v1', 'painting'],
+      },
+      handler: function (request, reply) {
+        return Painting.find ();
+      },
+    },
 
-init();
+    {
+      method: 'post',
+      path: '/api/v1/paintings',
+      config: {
+        description: 'post painting',
+        tags: ['api', 'v1', 'painting'],
+      },
+      handler: function (request, reply) {
+        const {name, url, techniques} = request.payload;
+        const painting = new Painting ({
+          name,
+          url,
+          techniques,
+        });
+        return painting.save ();
+      },
+    },
+  ]);
+
+  await server.start ();
+  console.log (`server running on ${server.info.uri}`);
+};
+
+init ();
